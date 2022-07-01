b9d6fbf205f9096b859d8e03920a286bd530cd76
aidan-starke <starke.aidan@gmail.com>
2022-07-01 20:56:30 +1200

    Implement & test LRUCache for HashMap

--- src/lib.rs
+++ src/lib.rs
@@ -9,23 +9,57 @@ trait LRUCache<K: Eq + Hash + Ord, V> {
     /// Initialize the cache with `max` items stored
     fn initialize(max: usize) -> Self;
     /// Retrieve the value stored by key, if it exists
-    fn get(&self, key: K) -> Option<&V>;
+    fn get(&mut self, key: K) -> Option<&V>;
     /// Store a `value` under `key`
     fn set(&mut self, key: K, value: V);
 }
 
-impl<K: Eq + Hash + Ord, V> LRUCache<K, V> for HashMap<K, V> {
-    fn initialize(max: usize) -> Self {
-        let map: HashMap<K, V> = HashMap::with_capacity(max);
+pub struct Cache<K: Eq + Hash, V> {
+    accessed: Vec<K>,
+    map: HashMap<K, V>,
+    max_size: usize,
+}
 
-        map
+impl<K: Copy + Eq + Hash + Ord, V> LRUCache<K, V> for Cache<K, V> {
+    fn initialize(max: usize) -> Self {
+        let map: HashMap<K, V> = HashMap::new();
+        let accessed: Vec<K> = Vec::new();
+
+        Cache {
+            accessed,
+            map,
+            max_size: max,
+        }
     }
 
-    fn get(&self, key: K) -> Option<&V> {
-        self.get(&key)
+    fn get(&mut self, key: K) -> Option<&V> {
+        if self.accessed.len() > self.max_size {
+            let accessed_key = self.accessed.remove(0);
+            self.map.remove(&accessed_key);
+        }
+
+        self.map.get(&key)
     }
 
     fn set(&mut self, key: K, value: V) {
-        self.insert(key, value);
+        self.accessed.push(key.clone());
+        self.map.insert(key, value);
     }
+}
+
+#[test]
+fn cache_clears() {
+    let mut cache = Cache::<&str, &str>::initialize(2);
+
+    cache.set("one", "uno");
+    cache.set("two", "dos");
+    cache.set("three", "tres");
+
+    cache.get("uno");
+    cache.get("dos");
+    cache.get("tres");
+
+    assert_eq!(cache.map.len(), 2);
+    assert_eq!(cache.accessed.len(), 2);
+    assert_eq!(cache.get("uno"), None);
 }
